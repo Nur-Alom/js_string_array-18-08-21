4f253bc0c41115babedf511bf4f07567dad2792a
Nur-Alom <alamn7150@gmail.com>
2021-08-18 23:58:18 +0600

    first commit

---
+++ case-convert.js
@@ -0,0 +1,17 @@
+const anthem = 'Amer Sonar Bangla Ami Tomai Valobasi';
+const search = 'valo';
+
+
+const userInput = 'blackPinK';
+const savedUserName = 'blackPink';
+// console.log(userInput.toLowerCase());
+console.log(savedUserName.toUpperCase());
+
+if (userInput.toLowerCase() == savedUserName.toLowerCase()) {
+    console.log('common');
+}
+else {
+    console.log('failed');
+}
+
+
